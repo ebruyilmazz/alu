c3daa9e4e7a12d7412373160b62cc8d486ff5f28
ebruyilmazz <103778178+ebruyilmazz@users.noreply.github.com>
2022-04-22 01:08:00 +0300

    alu

---
+++ mimari/mimari/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace mimari
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(66, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(66, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(66, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(238, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(643, 49);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.Location = new System.Drawing.Point(66, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(135, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox5.Location = new System.Drawing.Point(66, 184);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(66, 210);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(135, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(467, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox7.Location = new System.Drawing.Point(467, 49);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(135, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox8.Location = new System.Drawing.Point(467, 76);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(135, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(467, 103);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(135, 20);
+            this.textBox9.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ mimari/mimari/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mimari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Toplama")
+            {
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int sonuc = sayi1 + sayi2;
+                textBox3.Text = (Convert.ToString(sonuc));
+            }
+            else if (comboBox1.Text == "Çıkarma")
+            {
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int sonuc = sayi1 - sayi2;
+                textBox3.Text = (Convert.ToString(sonuc));
+            }
+            else if (comboBox1.Text == "Bölme")
+            {
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int sonuc = sayi1 / sayi2;
+                textBox3.Text = (Convert.ToString(sonuc));
+            }
+            else if (comboBox1.Text == "Çarpma")
+            {
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int sonuc = sayi1 * sayi2;
+                textBox3.Text = (Convert.ToString(sonuc));
+            }
+
+            if (comboBox1.Text == "VE")
+            {
+                string binary = " ";
+                string ikilik = " ";
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int toplam = sayi1 + sayi2;
+                textBox3.Text = Convert.ToString(toplam);
+
+                while (sayi1 != 0)
+                {
+                    int sonuc = sayi1 % 2;
+                    sayi1 = sayi1 / 2;
+                    ikilik = Convert.ToString(sonuc) + ikilik;
+
+                }
+                while (sayi2 != 0)
+                {
+                    int sonuc = sayi2 % 2;
+                    sayi2 = sayi2 / 2;
+                    binary = Convert.ToString(sonuc) + binary;
+                }
+                textBox4.Text = binary;
+                textBox5.Text = ikilik;
+                string degisken = " ";
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = a & b;
+
+                while (c != 0)
+                {
+                    int x = c % 2;
+                    c = c / 2;
+                    degisken = Convert.ToString(x) + degisken;
+
+                }
+                textBox6.Text = degisken;
+
+            }
+
+            if (comboBox1.Text == "VEYA")
+            {
+                string binary=" ";
+                string ikilik = " ";
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int toplam = sayi1 + sayi2;
+                textBox3.Text= Convert.ToString(toplam);
+
+                while (sayi1 != 0)
+                {
+                    int sonuc = sayi1 % 2;
+                    sayi1 = sayi1 / 2;
+                    ikilik = Convert.ToString(sonuc) + ikilik;
+
+                }
+                while (sayi2 != 0)
+                {
+                    int sonuc = sayi2 % 2;
+                    sayi2 = sayi2 / 2;
+                    binary = Convert.ToString(sonuc) + binary;
+                }
+                textBox4.Text = binary;
+                textBox5.Text = ikilik;
+                string degisken = " ";
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = a | b;
+
+                while (c != 0)
+                {
+                    int x = c % 2;
+                    c = c / 2;
+                    degisken = Convert.ToString(x) + degisken;
+
+                }
+                textBox6.Text = degisken;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Toplama");
+            comboBox1.Items.Add("Çıkarma");
+            comboBox1.Items.Add("Bölme");
+            comboBox1.Items.Add("Çarpma");
+            comboBox1.Items.Add("VE");
+            comboBox1.Items.Add("VEYA");
+
+            comboBox2.Items.Add("VE");
+            comboBox2.Items.Add("VEYA");
+            comboBox2.Items.Add("XOR");
+            comboBox2.Items.Add("XNOR");
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "VEYA")
+            {
+
+                string birinci = Convert.ToString(textBox7.Text);
+                string ikinci = Convert.ToString(textBox8.Text);
+
+
+                string[] dizi = new string[birinci.Length];
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    if (birinci[i] == '1' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '1' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "1";
+                    }
+
+                }
+                string cevap = "";
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    cevap = cevap + dizi[i];
+                }
+
+                textBox9.Text = cevap;
+            }
+
+            else if (comboBox2.Text == "VE")
+            {
+
+                string birinci = Convert.ToString(textBox7.Text);
+                string ikinci = Convert.ToString(textBox8.Text);
+
+
+                string[] dizi = new string[birinci.Length];
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    if (birinci[i] == '1' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '1' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "0";
+                    }
+
+                }
+                string cevap = "";
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    cevap = cevap + dizi[i];
+                }
+
+                textBox9.Text = cevap;
+            }
+
+            else if (comboBox2.Text == "XNOR")
+            {
+
+                string birinci = Convert.ToString(textBox7.Text);
+                string ikinci = Convert.ToString(textBox8.Text);
+
+
+                string[] dizi = new string[birinci.Length];
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    if (birinci[i] == '1' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '1' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "0";
+                    }
+
+                }
+                string cevap = "";
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    cevap = cevap + dizi[i];
+                }
+
+                textBox9.Text = cevap;
+            }
+
+            else if (comboBox2.Text == "XOR")
+            {
+
+                string birinci = Convert.ToString(textBox7.Text);
+                string ikinci = Convert.ToString(textBox8.Text);
+
+
+                string[] dizi = new string[birinci.Length];
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    if (birinci[i] == '1' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '1' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "1";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '0')
+                    {
+                        dizi[i] = "0";
+                    }
+                    if (birinci[i] == '0' && ikinci[i] == '1')
+                    {
+                        dizi[i] = "1";
+                    }
+
+                }
+                string cevap = "";
+
+                for (int i = 0; i < birinci.Length; i++)
+                {
+                    cevap = cevap + dizi[i];
+                }
+
+                textBox9.Text = cevap;
+            }
+        }
+
+   
+    }
+
+    
+}
